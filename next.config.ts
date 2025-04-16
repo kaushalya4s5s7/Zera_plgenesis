c35b07d681308097b4f6c27a77d154fca89d98bd
Sarthak <sarthakbansalog@gmail.com>
2025-04-16 22:33:56 +0530

    bkc

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,9 @@ const nextConfig: NextConfig = {
   env: {
     MISTRAL_API_KEY: process.env.MISTRAL_API_KEY,
   },
+  export:"outer",
   images: {
+    unoptimized: true,
     domains: ["images.unsplash.com", "assets.aceternity.com"],
     remotePatterns: [
       {
