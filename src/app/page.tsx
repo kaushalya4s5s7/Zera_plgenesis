253ad8eeb65a03cfc50b2cbe35ea8447ed53ad4e
kaushalya4s5s7 <chaudharikaushal02@gmail.com>
2025-04-19 12:03:27 +0530

    feat(featurecarousel): added the feature Carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import CTA from "@/components/home/CTA";
 import WobbleCardDemo from "@/components/home/WobbleCardDemo";
 import Footer from "@/components/home/footer";
 import FeaturesSectionDemo from "@/components/home/featuresdemo";
+import FeatureCarousel from "@/components/home/FeatureCorousel";
 
 const HomePage = () => {
   // const movingStripItems = [
@@ -26,25 +27,18 @@ const HomePage = () => {
         <WobbleCardDemo />
         <FeaturesSectionDemo />
         <Features />
-        {/* <FeatureCarousel
-          title="How SexAudit Works"
+        <FeatureCarousel
+          title="How Zera Works"
           description="All in one Solution"
           image={{
-            step1light1:
-              "https://img.freepik.com/free-vector/www-internet-globe-grid_78370-2008.jpg?t=st=1744798138~exp=1744801738~hmac=5ef7077648da04c0840864405aa2cadb9fe5f9b9d6a433916c35314631516ca9&w=900",
-            step1light2:
-              "https://img.freepik.com/free-vector/cyber-security-concept_23-2148543851.jpg?t=st=1744798200~exp=1744801800~hmac=3f19249dd5f49c30e99c41f28336e601c50051410eb03e3e937a97bfa9ea00f4&w=900",
-            step2light1:
-              "https://img.freepik.com/free-vector/gradient-international-internet-day-illustration_23-2150741460.jpg?t=st=1744798250~exp=1744801850~hmac=1d7b41959c6ae267760d68bb0135a6e4c601b31483865524da3d7c65c8ccc9f2&w=900",
-            step2light2:
-              "https://img.freepik.com/free-vector/gradient-international-internet-day-illustration_23-2150741460.jpg?t=st=1744798250~exp=1744801850~hmac=1d7b41959c6ae267760d68bb0135a6e4c601b31483865524da3d7c65c8ccc9f2&w=900",
-            step3light:
-              "https://img.freepik.com/free-vector/gradient-international-internet-day-illustration_23-2150741460.jpg?t=st=1744798250~exp=1744801850~hmac=1d7b41959c6ae267760d68bb0135a6e4c601b31483865524da3d7c65c8ccc9f2&w=900",
-            step4light:
-              "https://img.freepik.com/free-vector/gradient-international-internet-day-illustration_23-2150741460.jpg?t=st=1744798250~exp=1744801850~hmac=1d7b41959c6ae267760d68bb0135a6e4c601b31483865524da3d7c65c8ccc9f2&w=900",
+            step1light1: "./auditor.PNG",
+            step1light2: "./builder.PNG",
+            step2light1: "./Testor.PNG",
+            step3light: "./auditor.PNG",
+            step4light: "./builder.PNG",
             alt: "Feature showcase",
           }}
-        /> */}
+        />
         {/* <Testimonials /> */}
         <CTA />
       </main>
